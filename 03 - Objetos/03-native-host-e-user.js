9a939f861698df8b4dd49f84224eadcada9d1c62
daniiantunes <daniel.viantunes@gmail.com>
2021-07-06 16:59:14 -0300

    03 - Objetos - pt1

---
+++ 03 - Objetos/03-native-host-e-user.js
@@ -0,0 +1,19 @@
+// Liste 5 objetos nativos
+Object
+String
+Number
+Boolean
+Function
+Array
+Math
+Date
+
+// Liste 5 objetos do browser
+Window
+Document
+History
+NodeList
+HTMLCollection
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
